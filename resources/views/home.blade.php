d82455af04113061cbcd43dfb2a1ce57572b9da4
ShioriHasegawa2629 <shasegawa995@gmail.com>
2025-11-21 22:15:30 +0900

    Now UI language is automatically selected depending on the 'users' table column 'target_language'.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('messages.welcome') }}
                 </div>
             </div>
         </div>
