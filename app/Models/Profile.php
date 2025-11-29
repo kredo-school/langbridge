8eb95e285b3458aec66165c05bfc655aaca1606c
ShioriHasegawa2629 <shasegawa995@gmail.com>
2025-11-29 14:51:01 +0900

    ?

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Support\Str; // ここはクラスの外で use
 use App\Models\User;
 use App\Models\Interest;
 use Illuminate\Support\Str;
