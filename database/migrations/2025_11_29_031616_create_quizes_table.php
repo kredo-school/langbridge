1e6ca625c5345dd5aeb70f03ccc3ac67e6ef961a
ShioriHasegawa2629 <shasegawa995@gmail.com>
2025-11-29 12:19:19 +0900

    made migrations

---
+++ database/migrations/2025_11_29_031616_create_quizes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quizes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('vocabulary_id');
+            $table->boolean('is_correct');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+
+            $table->foreign('vocabulary_id')
+                  ->references('id')
+                  ->on('vocabularies')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quizes');
+    }
+};
