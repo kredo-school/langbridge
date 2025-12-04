34162d9456f12f8de11524b79bb3a44ce241dcfe
Yumi191 <yumi7819@gmail.com>
2025-12-04 21:41:41 +0900

    add interest to profile page and update home blade and controller

---
+++ config/interests.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'Entertainment',
+    'Sports & Fitness',
+    'Food & Drink',
+    'Travel',
+    'Education & Learning',
+    'Technology',
+    'Fashion & Beauty',
+    'Business & Finance',
+    'Art & Design',
+    'Lifestyle & Wellness',
+    'Family & Relationships',
+    'Gaming',
+    'Social Causes',
+    'Shopping & Deals',
+    'Pets & Animals',
+    'Books & Literature',
+];
